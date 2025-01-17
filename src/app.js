38169c7b1919bc4f17436db797dbb31926d69aa3
vivek-singh-rajawa <viveksinghrajawat62@gmail.com>
2025-01-17 18:11:14 +0530

    Data sanitization

--- src/app.js
+++ src/app.js
@@ -39,6 +39,30 @@ app.delete("/user", async (req, res) => {
   }
 })
 
+app.patch("/user/:userId", async (req, res) => {
+  const userId = req.params?.userId;
+  const data = req.body;
+  
+  
+  try {
+
+    const ALLOWED_UPDATE = ["firstName", "lastName", "password", "age", "gender", "photoUrl", "about", "skills"];
+  const updates = Object.keys(data).every((update) => ALLOWED_UPDATE.includes(update));
+
+  if (!updates) {
+    throw new Error("Invalid updates");
+  }
+  
+    const user = await User.findByIdAndUpdate(userId, req.body,{
+      returnDocument: "after",
+      runValidators: true
+  });
+  res.send(user)
+  } catch (error) { 
+    res.status(400).send("UPdate failed:" + error.message);
+  }
+});
+
 connectDB()
   .then(() => {
      console.log("MongoDB Connected");
--- src/models/user.js
+++ src/models/user.js
@@ -2,24 +2,46 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     firstName: {
-        type : String
+        type : String,
+        required : true
     },
     lastName: {
         type: String
     },
     emailId: {
-        type: String
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true
     },
     password: {
-        type: String
+        type: String,
+        required: true
     },
     age: {
         type: Number
     },
     gender: {
-        type: String
-    }
-});
+        type: String,
+        enum: ["male","female","other"]
+    },
+    photoUrl: {
+        type: String,
+        default: "https://elitebusinessmagazine.co.uk/wp-content/uploads/2020/06/Anonymous-Male-Writer-EliteBusinessMagazine-3.jpg"
+    },
+    about: {
+        type: String,
+        default: "Hey there! I am using this app"
+    },
+    SKills: {
+        type: Array
+    },
+},
+{
+    timestamps: true
+}
+);
 
 module.exports = mongoose.model("User", userSchema);
 
